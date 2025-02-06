f23adb85e1adb626081a33d4327aaa13465b25ad
Bipin Singh <bipinsingh8052@gmail.com>
2025-02-06 19:39:15 +0530

    completed

--- backend/Controller/Employee.js
+++ backend/Controller/Employee.js
@@ -18,11 +18,54 @@ const InsertPage=async(req,res)=>{
     const data = await ModelEmploye.create({
         name,age,EmployeeId,salary
     })
+    res.json({msg:"Data is success saved"})
+}
+
+
+
+const SearchPage =async(req,res)=>{
+    const {name}=req.body;
+    console.log(name);
+    const data = await ModelEmploye.find({name:name})
     res.send(data)
 }
+
+const DeletePage=async(req,res)=>{
+    console.log(req.query);
+    const{emp}=req.query;
+    console.log(emp,"id")
+    await ModelEmploye.findByIdAndDelete(emp);
+    res.send("Deleted that Employeee Data")
+}
+
+
+const updateDataPage=async(req,res)=>{
+    // console.log(req.body);
+    const { empid}=req.body;
+    const data = await ModelEmploye.findById(empid)
+    console.log(data,"hanan")
+    res.send(data)
+   
+    // res.send("updata")
+}
+
+const UpdateSave=async(req,res)=>{
+    // console.log(req.body);
+    const{_id}=req.body;
+    console.log(_id)
+    await ModelEmploye.findByIdAndUpdate(_id,req.body)
+    res.send("Data is UpDated !!!")
+}
+
+
 
 module.exports={
     homePage,
     DisplayPage,
-    InsertPage
+    InsertPage,
+    SearchPage,
+    DeletePage,
+    updateDataPage,
+    UpdateSave
+
 }
--- backend/Routes/Employess.js
+++ backend/Routes/Employess.js
@@ -5,6 +5,8 @@ const ContollerEmploy =require("../Controller/Employee");
 route.get("/home",ContollerEmploy.homePage);
 route.get("/display" ,ContollerEmploy.DisplayPage);
 route.post("/insert",ContollerEmploy.InsertPage);
-
-
+route.post("/search",ContollerEmploy.SearchPage);
+route.get("/delete" ,ContollerEmploy.DeletePage)
+route.post("/updateData",ContollerEmploy.updateDataPage);
+route.post("/updatesave" ,ContollerEmploy.UpdateSave)
 module.exports=route;
